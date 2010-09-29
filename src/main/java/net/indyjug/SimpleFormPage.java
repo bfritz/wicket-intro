cb60590a32824a7ab278eeb70724e1388ad7713a
Brad Fritz <brad@fewerhassles.com>
2010-09-29 14:55:36 -0400

    add a form to the simple form page

--- src/main/java/net/indyjug/SimpleFormPage.java
+++ src/main/java/net/indyjug/SimpleFormPage.java
@@ -1,6 +1,9 @@
 package net.indyjug;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
 /**
  * Simple demo form.
@@ -9,4 +12,19 @@ public class SimpleFormPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
+	public SimpleFormPage() {
+		final Model<String> somethingModel = Model.of("blah");
+
+		Form<Void> f = new Form<Void>("myForm") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onSubmit() {
+				System.out.println("User entered: " + somethingModel.getObject());
+			}
+		};
+
+		f.add(new TextField<String>("something", somethingModel));
+		add(f);
+    }
 }
