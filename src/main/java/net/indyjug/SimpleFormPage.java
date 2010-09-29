fcb32f2a367fadf36465c4c3497afff906771d5a
Brad Fritz <brad@fewerhassles.com>
2010-09-29 15:25:18 -0400

    add AJAX-enabled text field

--- src/main/java/net/indyjug/SimpleFormPage.java
+++ src/main/java/net/indyjug/SimpleFormPage.java
@@ -1,5 +1,7 @@
 package net.indyjug;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
@@ -26,6 +28,17 @@ public class SimpleFormPage extends WebPage {
 		};
 
 		f.add(new TextField<String>("something", somethingModel));
+
+		TextField<String> atf = new TextField<String>("ajaxTextField", somethingModel);
+		atf.add(new AjaxFormComponentUpdatingBehavior("onkeyup") {
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				System.out.println("changed");
+			}
+
+		});
+		f.add(atf);
+
 		add(f);
 
 		add(new Label("said", somethingModel));
